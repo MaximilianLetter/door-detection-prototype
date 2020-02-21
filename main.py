4bdf593e8affe0bb33a747e088f08aeb59d71064
Maximilian Letter <maximilian.letter@gmail.com>
2020-02-21 09:39:11 +0100

    Experiment with multiple approaches

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import cv2
 import sys
 import numpy as np
+import math
 
 
 def detect(img):
-    img = resize(img, 500)
+    img = resize(img, 240)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    blurred = cv2.GaussianBlur(gray, (5,5), 0.8)
 
     # Auto thresholds for now
     sigma = 0.33
-    v = np.median(gray)
+    v = np.median(blurred)
     lower = int(max(0, (1.0 - sigma) * v))
     upper = int(min(255, (1.0 + sigma) * v))
 
-    edges = cv2.Canny(gray, lower, upper)
+    edges = cv2.Canny(blurred, lower, upper)
 
     # OPTION 1: Hough Transform for extracting lines
     # img = houghOperations(img, edges)
@@ -23,21 +26,152 @@ def detect(img):
     # img = lsdOperations(gray)
 
     # Option 3: FLD FastLineDetector
-    # img = fldOperations(gray)
+    img = fldOperations(gray)
 
     # Option 4: Saliency [multiple saliency objects exist]
     # sal = cv2.saliency.StaticSaliencySpectralResidual_create()
     # sal = cv2.saliency.StaticSaliencyFineGrained_create()
     # success, img = sal.computeSaliency(img)
 
+    # Option 5: Corners
+    # img = useCorners(img, edges)
+
     return img
+
+def useCorners(img, edges):
+    corners = cv2.goodFeaturesToTrack(edges, 100, 0.1, 10)
+    for c in corners:
+        x, y = c.ravel()
+        cv2.circle(img, (x, y), 3, (0, 0, 255), -1)
+    print(len(corners))
+
+    # TODO
+    c_groups = groupCorners(corners)
+
+    print(len(c_groups))
+    for g in c_groups[0]:
+        for c in g:
+            x, y = c.ravel()
+            cv2.circle(img, (x, y), 3, (0, 255, 0), -1)
+
+    return img
+
+# TODO
+def groupCorners(corners):
+    groups = []
+    # Grouping algorithm not imlemented yet
+    for c1 in corners:
+        x1, y1 = c1.ravel()
+        group = [c1]
+        for c2 in corners:
+            x2, y2 = c2.ravel()
+
+            size = math.sqrt((x2-x1)**2 + (y2-y1)**2)/400
+            if size > 1 or size < 0:
+                continue
+
+            # dir = np.cross(math.atan(abs(x2-x1)/abs(y2-y1)), 180/np.pi)
+            if (y2-y1) == 0:
+                dir = 0
+            else:
+                dir = math.atan(abs(x2-x1)/abs(y2-y1)) * 180/np.pi
+            if dir > 90 or dir < 0:
+                continue
+
+            group.append(c2)
+            if len(group) == 4:
+                break
+
+        groups.append(group)
+
+    return groups
 
 def fldOperations(img):
     fld = cv2.ximgproc.createFastLineDetector()
     lines = fld.detect(img)
     img = fld.drawSegments(img, lines)
 
+    # Line Selection naive algorithm
+    h, w = img.shape[:2]
+
+    candidates = findRectFromLines(lines, w, h)
+
+    for c in candidates:
+        pts = np.array([c], np.int32)
+        pts = pts.reshape((-1,1,2))
+        cv2.polylines(img, [pts], True, (0,255,255), 1, cv2.LINE_AA)
+    # candidates = np.array(candidates)
+    # img = fld.drawSegments(img, candidates)
+
     return img
+
+def findRectFromLines(lines, w, h):
+    """
+    Naive algorithm for detecting a rectangle.
+    Selection when multiple candidates are found is missing.
+    Due not merged lines most results do not work out.
+    Also only working on frontal images of doors.
+    """
+    MIN_DIST = w / 4
+    HOR_THRESH = 0.1
+    VERT_THRESH = 2.0
+    POINT_THRESH = 5.0
+
+    candidates = []
+
+    for line in lines:
+        door_corners = []
+
+        x1, y1, x2, y2 = line.ravel()
+        dist = np.sqrt((x1-x2)**2 + (y1-y2)**2)
+        if x1 != x2:
+            m = abs((y2-y1) / (x2-x1))
+        else:
+            m = float("inf")
+
+        # Horizontal line found
+        if dist > MIN_DIST and m < HOR_THRESH:
+            door_corners.append([x1, y1])
+            door_corners.append([x2, y2])
+
+            height = (y1+y2) / 2
+
+            for line_vert in lines:
+                x1_v, y1_v, x2_v, y2_v = line_vert.ravel()
+
+                dist = np.sqrt((x1_v-x2_v)**2 + (y1_v-y2_v)**2)
+                if x1_v != x2_v:
+                    m = abs((y2_v-y1_v) / (x2_v-x1_v))
+                else:
+                    m = float("inf")
+
+                if dist > (MIN_DIST * 1.5) and m > VERT_THRESH:
+                    # Check if points are possible connection points
+                    dist_x11 = abs(x1-x1_v)
+                    dist_y11 = abs(y1-y1_v)
+
+                    dist_x12 = abs(x1-x2_v)
+                    dist_y12 = abs(y1-y2_v)
+
+                    dist_x21 = abs(x2-x1_v)
+                    dist_y21 = abs(y2-y1_v)
+
+                    dist_x22 = abs(x2-x2_v)
+                    dist_y22 = abs(y2-y2_v)
+
+                    if (dist_x11 < POINT_THRESH and dist_y11 < POINT_THRESH) or (dist_x21 < POINT_THRESH and dist_y21 < POINT_THRESH):
+                        if y2_v > height:
+                            door_corners.append([x2_v, y2_v])
+
+                    if (dist_x12 < POINT_THRESH and dist_y12 < POINT_THRESH) or (dist_x22 < POINT_THRESH and dist_y22 < POINT_THRESH):
+                        if y1_v > height:
+                            door_corners.append([x1_v, y1_v])
+
+                if len(door_corners) == 4:
+                    candidates.append(door_corners)
+                    door_corners = [[x1, y1], [x2, y2]]
+
+    return candidates
 
 def lsdOperations(img):
     lsd = cv2.createLineSegmentDetector(0)
@@ -56,7 +190,7 @@ def houghOperations(img, edges):
     # img = showLines(img, hor_lines)
 
     # Experiment extracting vertical and horizontal lines, probabilistic Hough Transform
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 10, 100, 5)
+    lines = cv2.HoughLinesP(edges, 5, np.pi/180, 10, 50, 5)
 
     # straight_lines = [l for l in lines if direction(l[0][0], l[0][1], l[0][2], l[0][3]) == 0]
     # img = showLines(img, straight_lines)
